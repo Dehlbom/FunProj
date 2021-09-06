805453c61af8ceb27c01dc9c80e23f9f11b35e07
Gustaf <gustafdehlbom@gmail.com>
2021-09-06 22:20:28 +0200

    rps v1

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
-#21:44
-#21:45
-print('Hello World')
+from random import randint
+condition = True
 
-print('Hello Gustaf')
+
+while condition:
+    try:
+        amount_players = int(input('1. Play vs AI. \n'
+                               '2. Simulate rock, paper & sissorcs \n'
+                               '3. Quit \n'))
+        if amount_players == 3:
+            condition = False
+        elif amount_players == 1:
+            pass
+        elif amount_players == 2:
+            pass
+    except ValueError as err:
+            print("Oh no!, that is not a valid value. Try again...")
+            print("({})".format(err))
+
+
